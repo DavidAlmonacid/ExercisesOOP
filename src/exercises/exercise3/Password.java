bbb6ef08b772dba07be7c545f0d1aad44c3ac4af
David Almonacid <david.almonacidf@gmail.com>
2024-11-30 13:07:54 -0500

    Add Password class with password generation logic and main class for demonstration

---
+++ src/exercises/exercise3/Password.java
@@ -0,0 +1,97 @@
+/**
+ * Haz una clase llamada Password que siga las siguientes condiciones:
+ *
+ * Que tenga los atributos longitud y contraseña. Por defecto, la longitud sera de 12.
+ *
+ * Los constructores serán los siguiente:
+ * Un constructor por defecto.
+ * Un constructor con la longitud que nosotros le pasemos. Generará una contraseña aleatoria con esa longitud.
+ *
+ * Los métodos que implementa serán:
+ * generarPassword(): Genera la contraseña del objeto con la longitud que tenga.
+ * Método get para contraseña y longitud.
+ * Método set para longitud.
+ */
+
+package exercises.exercise3;
+
+public class Password {
+
+    private int length;
+    private String password;
+
+    public Password() {
+        this.length = 12;
+        this.password = generatePassword();
+    }
+
+    public Password(int length) {
+        if (length < 12) {
+            throw new IllegalArgumentException("Length must be greater than or equal to 12");
+        }
+
+        this.length = length;
+        this.password = generatePassword();
+    }
+
+    private String generatePassword() {
+        final String CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        final String NUMBERS = "0123456789";
+
+        String[] sortedPasswordChars = new String[length];
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex;
+            String currentChar;
+
+            if (i < length / 2) {
+                randomIndex = (int) (Math.random() * NUMBERS.length());
+                currentChar = Character.toString(NUMBERS.charAt(randomIndex));
+
+                sortedPasswordChars[i] = currentChar;
+            } else {
+                randomIndex = (int) (Math.random() * CHARS.length());
+                currentChar = Character.toString(CHARS.charAt(randomIndex));
+
+                sortedPasswordChars[i] = (i % 2 == 0)
+                        ? currentChar.toLowerCase()
+                        : currentChar;
+            }
+        }
+
+        return Shuffle(sortedPasswordChars);
+    }
+
+    private String Shuffle(String[] sortedPasswordChars) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < sortedPasswordChars.length; i++) {
+            int randomIndex = (int) (Math.random() * sortedPasswordChars.length);
+
+            String temp = sortedPasswordChars[i];
+            sortedPasswordChars[i] = sortedPasswordChars[randomIndex];
+            sortedPasswordChars[randomIndex] = temp;
+        }
+
+        for (String character : sortedPasswordChars) {
+            sb.append(character);
+        }
+
+        return sb.toString();
+    }
+
+    // Getters
+    public int getLength() {
+        return length;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // Setters
+    public void setLength(int length) {
+        this.length = length;
+        this.password = generatePassword();
+    }
+}
