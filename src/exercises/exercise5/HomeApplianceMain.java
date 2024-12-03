52bf7fc62b73d9ddd9bf6b5bef14fcb9ec83a297
David Almonacid <david.almonacidf@gmail.com>
2024-12-03 11:02:39 -0500

    Complete exercise 5

---
+++ src/exercises/exercise5/HomeApplianceMain.java
@@ -0,0 +1,52 @@
+/**
+ * Ahora crea una clase ejecutable que realice lo siguiente:
+ * Crea un array de Electrodomésticos de 6 posiciones.
+ * Asigna a cada posición un objeto de las clases anteriores con los valores que
+ * desees.
+ * <p>
+ * Ahora, recorre este array y ejecuta el método precioFinal().
+ * Deberás mostrar el precio de cada clase, es decir, el precio de todas las
+ * televisiones por un lado, el de las lavadoras por otro y la suma de los
+ * Electrodomésticos (puedes crear objetos Electrodoméstico, pero recuerda que
+ * Television y Lavadora también son electrodomésticos). Recuerda el uso
+ * operador instanceof.
+ * <p>
+ * Por ejemplo, si tenemos un Electrodoméstico con un precio final de 300, una
+ * lavadora de 200 y una televisión de 500, el resultado final será de 1000
+ * (300+200+500) para electrodomésticos, 200 para lavadora y 500 para
+ * televisión.
+ */
+
+package exercises.exercise5;
+
+public class HomeApplianceMain {
+    public static void main(String[] args) {
+        HomeAppliance[] homeAppliances = new HomeAppliance[6];
+        homeAppliances[0] = new Television(50, true, 100, Color.BLACK, EnergyConsumption.A, 10);
+        homeAppliances[1] = new WashingMachine(10, 200, Color.RED, EnergyConsumption.B, 20);
+        homeAppliances[2] = new Television();
+        homeAppliances[3] = new WashingMachine();
+        homeAppliances[4] = new Television(250, 40);
+        homeAppliances[5] = new WashingMachine(350, 50);
+
+        int totalTelevisionsPrice = 0;
+        int totalWashingMachinesPrice = 0;
+        int totalHomeAppliancesPrice = 0;
+
+        for (HomeAppliance homeAppliance : homeAppliances) {
+            if (homeAppliance instanceof Television) {
+                totalTelevisionsPrice += homeAppliance.getPrice();
+                System.out.println("Television price: " + homeAppliance.getPrice());
+            } else if (homeAppliance instanceof WashingMachine) {
+                totalWashingMachinesPrice += homeAppliance.getPrice();
+                System.out.println("WashingMachine price: " + homeAppliance.getPrice());
+            }
+
+            totalHomeAppliancesPrice += homeAppliance.getPrice();
+        }
+
+        System.out.println("\nTotal Televisions price: " + totalTelevisionsPrice);
+        System.out.println("Total WashingMachines price: " + totalWashingMachinesPrice);
+        System.out.println("Total HomeAppliances price: " + totalHomeAppliancesPrice);
+    }
+}
---
+++ src/exercises/exercise5/Television.java
@@ -0,0 +1,69 @@
+/**
+ * Crearemos una subclase llamada Television con las siguientes características:
+ * Sus atributos son resolución (en pulgadas) y sintonizador TDT (booleano),
+ * además de los atributos heredados.
+ * Por defecto, la resolución será de 20 pulgadas y el sintonizador será, false.
+ * <p>
+ * Los constructores que se implementaran serán:
+ * - Un constructor por defecto.
+ * - Un constructor con el precio y peso. El resto por defecto.
+ * - Un constructor con la resolución, sintonizador TDT y el resto de atributos
+ * heredados. Recuerda que debes llamar al constructor de la clase padre.
+ * <p>
+ * Los métodos que se implementara serán:
+ * - Método get de resolución y sintonizador TDT.
+ * - precioFinal(): Si tiene una resolución mayor de 40 pulgadas se incrementará
+ * el precio un 30%, si tiene un sintonizador TDT incorporado aumentará 50.
+ * Recuerda que las condiciones que hemos visto en la clase Electrodoméstico
+ * también deben afectar al precio.
+ */
+
+package exercises.exercise5;
+
+public class Television extends HomeAppliance {
+    private final int DEFAULT_RESOLUTION = 20;
+    private final boolean DEFAULT_DTT = false;
+
+    private final int resolution;
+
+    public int getResolution() {
+        return resolution;
+    }
+
+    private final boolean dtt;
+
+    public boolean isDtt() {
+        return dtt;
+    }
+
+    public Television() {
+        super();
+        this.resolution = DEFAULT_RESOLUTION;
+        this.dtt = DEFAULT_DTT;
+    }
+
+    public Television(int price, float weight) {
+        super(price, weight);
+        this.resolution = DEFAULT_RESOLUTION;
+        this.dtt = DEFAULT_DTT;
+    }
+
+    public Television(int resolution, boolean dtt, int price, Color color, EnergyConsumption energyConsumption, float weight) {
+        super(price, color, energyConsumption, weight);
+        this.resolution = resolution;
+        this.dtt = dtt;
+    }
+
+    @Override
+    public void finalPrice() {
+        super.finalPrice();
+
+        if (dtt) {
+            price += 50;
+        }
+
+        if (resolution > 40) {
+            price += (int) (price * 0.3);
+        }
+    }
+}
