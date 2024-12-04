a89b8d61d844732e8e0a035a688e758542ac1420
David Almonacid <david.almonacidf@gmail.com>
2024-12-04 17:48:13 -0500

    Complete exercise 9

--- src/App.java
+++ src/App.java
@@ -6,30 +6,6 @@ public class App {
 
 // https://www.discoduroderoer.es/ejercicios-propuestos-y-resueltos-programacion-orientado-a-objetos-java/
 
-// 6) Crear una clase Libro que contenga los siguientes atributos:
-// – ISBN
-
-// – Titulo
-
-// – Autor
-
-// – Número de páginas
-
-// Crear sus respectivos métodos get y set correspondientes para cada atributo.
-// Crear el
-
-// método toString() para mostrar la información relativa al libro con el
-// siguiente formato:
-
-// «El libro con ISBN creado por el autor tiene páginas»
-
-// En el fichero main, crear 2
-
-// objetos Libro (los valores que se quieran) y mostrarlos por pantalla.
-
-// Por último, indicar cuál de los 2 tiene más páginas.
-
-// Spoiler
 // 7) Vamos a realizar una clase llamada Raices, donde representaremos los
 // valores de una ecuación de 2º grado.
 // Tendremos los 3 coeficientes como atributos, llamémosles a, b y c.
